0ec584be6633637604234a9912f26544e6b45767
Harrison Chapman <hchap1@hotmail.com>
2024-10-03 17:43:17 +1000

    basic stripping done

--- src/main.rs
+++ src/main.rs
@@ -8,8 +8,27 @@ fn remove_equations(input: &str) -> String {
 }
 
 fn remove_punctuation(input: &str) -> String {
-    let re = Regex::new(r"[,.'-][^\s]").unwrap();
+    let re = Regex::new(r"[,\.'-:][^\s]*").unwrap();
     re.replace_all(input, "").to_string().to_lowercase()
+}
+
+fn remove_double_spaces(input: &str) -> String {
+    let re = Regex::new(r"  ").unwrap();
+    re.replace_all(input, " ").to_string()
+}
+
+fn remove_parenthesis(input: &str) -> String {
+    let re = Regex::new(r"\([^()]*\)").unwrap();
+    let mut result = input.to_string();
+    while re.is_match(&result) {
+        result = re.replace_all(&result, "").to_string();
+    }
+    result
+}
+
+fn remove_functions(input: &str) -> String {
+    let re = Regex::new(r"#\S+").unwrap();
+    re.replace_all(input, "").to_string()
 }
 
 fn main() {
@@ -20,7 +39,12 @@ fn main() {
             match read_to_string(&filepath) {
                 Ok(raw) => {
                     let document: Vec<String> = raw.lines().map(|x| x.to_string()).collect::<Vec<String>>();
-                    let doc_string: String = remove_punctuation(&remove_equations(&document.join(" ")));
+                    let mut doc_string: String = remove_parenthesis(&document.join(" "));
+                    doc_string = remove_equations(&doc_string);
+                    doc_string = remove_parenthesis(&doc_string);
+                    doc_string = remove_functions(&doc_string);
+                    doc_string = remove_punctuation(&doc_string);
+                    doc_string = remove_double_spaces(&doc_string);
                     println!("{doc_string}");
                 }
                 Err(_) => {
